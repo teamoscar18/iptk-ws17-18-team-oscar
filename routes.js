bfb9846ddd499134282c8f9f90201ececf9d4028
Mujahid Masood <mujahid.masood@stud.tu-darmstadt.de>
2017-11-11 18:24:18 +0100

    -removed unused controller

--- routes.js
+++ routes.js
@@ -1,17 +1,15 @@
 module.exports = function (app) {
     var user = require('./controllers/UserController');
-    var location = require('./controllers/LocationController');
-    var fileController = require('./controllers/FileController');
-    var friend = require('./controllers/FriendController');
 
 
     app.get('/', function (req, res, next) {
         return res.send("WELCOME TO Victor City Rest services");
     });
 
-    app.post('/register', user.register); //Register User
+
     app.post('/login', user.login);  // Authenticate User
-    app.get('/users/:email', user.getUserByEmail);  // Get User Profile By email
+    app.post('/register', user.register); //Register User
+    /*app.get('/users/:email', user.getUserByEmail);  // Get User Profile By email
     app.get('/forgotpassword/:email', user.forgotPassword);  // send forgot password at user email
     app.post('/resetpassword', user.resetPassword);  // resets password via verification code
     app.post('/updatepassword', user.changePassword);  // change password,provide old password,new password,email
@@ -28,7 +26,7 @@ module.exports = function (app) {
     app.post('/addfriend', friend.addFriend);
     app.post('/unfriend', friend.unFriend);
     app.get('/friends/:email', friend.getFriendListByEmail);
-    app.get('/friendlist/:email', friend.searchFriend);
+    app.get('/friendlist/:email', friend.searchFriend);*/
 
 
 
