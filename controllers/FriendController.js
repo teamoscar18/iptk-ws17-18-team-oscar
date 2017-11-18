3cc44805fe9bb7134502384c2d8caf17e5c5d37c
bilawal.aziz <bilawal.aziz@gmail.com>
2017-11-17 22:07:24 -0800

    added functionality for adding friends

--- controllers/FriendController.js
+++ controllers/FriendController.js
@@ -80,47 +80,7 @@ function friendController() {
         try {
             var options = {upsert: true, new: true};
             var query = { email: useremail };
-            users.findOneAndUpdate(
-                query,
-                {  $addToSet: { friends: friendemail } },
-                options
-                , function (err, result) {
-
-                console.log("result=" + result);
-                console.log("err=" + err);
-                if (err) {
-                    return res.send(response.setResponse(false, " Server encountered some error, please Try again! ", 400, err, "", ""));
-                } else if (result) {
-                    query = { useremail: useremail };
-                    friends.findOneAndUpdate(
-                        query,
-                        {  $set: { useremail: useremail } },
-                        options
-                        , function (err, result) {
-                            if (result) {
-                                console.log('added email address in user friendlist %s', result.firstName);
-                                var friend = users.findOne({email:friendemail},function(err,user){
-                                    if (user) {
-                                         console.log('friend exists in user table %s', user.email);
-                                        // friends.findOne({useremail:useremail}).populate(firend).exec(function (err, story) {
-                                        //     if (err)res.send(response.setResponse(false, err, 400, null, "", ""));
-                                        //     console.log('The authors age is %s', story.useremail);
-                                        //   });
-                                        //   console.log('friend exists in user table %s', story);                               
-                                        // return res.send(response.setResponse(true, " Fechting Friend successfull ", 200, result, "", ""));
-                                    } else {
-        
-                                        return res.send(response.setResponse(false, "findOne"+friendemail+" "+err, 400, null, "", ""));
-                                    }
-                                });
-                                //return res.send(response.setResponse(true, " Fechting Friend successfull ", 200, result, "", ""));
-                            } else {
-
-                                return res.send(response.setResponse(false, "findOneAndUpdate"+useremail+" => "+err, 400, null, "", ""));
-                            }
-                        });
-                } else return res.send(response.setResponse(false, "findOneAndUpdate"+err, 400, null, "", ""));
-            });
+           
         } catch (ex) {
             console.log("Exception:" + ex);
             return res.send(response.setResponse(false, "/addFriend:Exception Occured", 400, ex, "", ""));
