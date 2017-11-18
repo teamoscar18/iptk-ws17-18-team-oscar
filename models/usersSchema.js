5c9eb7c8080995c7aa647888b7e3b7a3b30e5ff6
bilawal.aziz <bilawal.aziz@gmail.com>
2017-11-17 22:32:45 -0800

    added functionality for adding friends

--- models/usersSchema.js
+++ models/usersSchema.js
@@ -57,7 +57,7 @@ module.exports = (function usersSchema() {
         verificationCode: {
             type: String
         },
-        friends: [{ type: ObjectId, ref: 'users' }],
+        friends: [{ type:  mongoose.Schema.Types.ObjectId, ref: 'users' }],
         rank: [{
             type: Number,
             default: 0
